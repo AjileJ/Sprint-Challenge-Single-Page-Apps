2e46aa969ee872af9c1af315eefbb65ee35dad9c
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-09-20 10:53:21 -0700

    attempted search form no luck

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,12 +1,54 @@
-import React, { useState } from "react";
-import {withFormik, Form, Field} from 'formik';
-import * as Yup from 'yup';
+// import React, { useState,useEffect } from "react";
+// import axios from 'axios';
+// import '../index.css';
 
-export default function SearchForm({values,errors, touched, status}) {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
-}
+// const [searchs, setSearches] = useState([]);
+// useEffect(() => {
+//   axios.get('https://rickandmortyapi.com/api/character/')
+//   .then(response => {
+//     const searchInfo = response.data.results;
+//     setSearches(searchInfo);
+//   })
+//   .catch(error => {
+//     console.log(error);
+//   })
+// }, []);
+
+// export default function SearchForm() {
+
+// const [searchTerm, setSearchTerm] = useState('');
+// const [seachResults, setSearchResults] = useState([]);
+
+// useEffect((searchInfo)=> {
+//   const results = searchInfo.filter(character => character.toLowerCase().includes(searchTerm)
+//   );
+// },[searchTerm]);
+
+// const handleChange = event => {
+//   setSearchTerm(event.target.value);
+// };
+// return(
+//   <div className ='searchh'>
+//   <form>
+//     <label for ='name'>Search: </label>
+//     <input 
+//     id="name"
+//     type='text'
+//     name='textfield'
+//     placeholder="Search"
+//     value={searchTerm}
+//     onChange={handleChange}/>
+//   </form>
+//   <div className ='character-listt'>
+//     <ul>
+//       {searchResults.map(character => (
+//         <li>{character}</li>
+//       ))}
+//     </ul>
+//   </div>
+//   </div>
+// )
+// }
+
+
+
