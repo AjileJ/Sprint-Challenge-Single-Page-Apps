6a0160277452d9951b5ad0420cf672efa30b00e5
AjileJ <53097022+AjileJ@users.noreply.github.com>
2019-09-20 10:58:49 -0700

    put in a styled component or two

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import CharacterList from './components/CharacterList';
 import WelcomePage from './components/WelcomePage';
 import {Route} from 'react-router-dom';
 // import SearchForm from './components/SearchForm';
+import styled from "styled-components";
 
-
+const Colors = styled.div`
+color: gold;
+`;
+const Colors2 = styled.div`
+color: green;
+`;
 
 export default function App() {
   return (
     <main>
+      <Colors>
       <Header />
+      </Colors>
       {/* <SearchForm /> */}
       <Route exact path='/' component={WelcomePage}/>
+      <Colors2>
       <CharacterList/>
+      </Colors2>
    </main>
   );
 }
